d1893ed69c2d783512f761ac21be5b39d120d29f
Mahalakshmi Raveenthiran <mahakavi1999@gmail.com>
2021-03-31 10:28:37 +0530

    MoviesApp

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+// import { useDeviceOrientation } from "@react-native-community/hooks";
 
 export default function App() {
+  // const { landscape } = useDeviceOrientation();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>MoviesApp</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    // height: landscape ? "100%" : "30%",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
